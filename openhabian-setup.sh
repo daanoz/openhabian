866331afdfcc3f18bd95d9c8680d831ca0ec5576
Daan Sieben <developer@daansieben.net>
2017-07-15 09:31:25 +0200

    Add support for arm64 based devices

--- openhabian-setup.sh
+++ openhabian-setup.sh
@@ -121,6 +121,12 @@ is_arm() {
     *) return 1 ;;
   esac
 }
+is_arm64() {
+  case "$(uname -m)" in
+    arm64|aarch64) return 0 ;;
+    *) return 1 ;;
+  esac
+}
 is_ubuntu() {
   [[ $(lsb_release -d) =~ "Ubuntu" ]]
   return $?
@@ -347,7 +353,7 @@ java_zulu_embedded() {
   echo "deb $_arch http://repos.azulsystems.com/$_distro stable main" > /etc/apt/sources.list.d/zulu-embedded.list
   cond_redirect apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 219BD9C9
   if [ $? -ne 0 ]; then echo "FAILED (keyserver)"; exit 1; fi
-  if is_pine64; then cond_redirect dpkg --add-architecture armhf; fi
+  if is_pine64 || is_arm64; then cond_redirect dpkg --add-architecture armhf; fi
   cond_redirect apt update
   cond_redirect apt -y install zulu-embedded-8
   if [ $? -eq 0 ]; then echo "OK"; else echo "FAILED"; exit 1; fi
